77f739e2a72ad2e70142a9172a05885128e6ba51
Florian Böhler <florian@boehlerbrothers.com>
2021-02-03 18:07:03 +0100

    add login demo

--- HomeRun/MainPage.xaml.cs
+++ HomeRun/MainPage.xaml.cs
@@ -4,15 +4,44 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
+
 using Xamarin.Forms;
 
 namespace HomeRun
 {
     public partial class MainPage : ContentPage
     {
+        public string WebApiKey = "AIzaSyBIGPP5wtt3bGl4aBMiuMsPiyJxVlbe9Js";
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var config = new FirebaseAuthConfig
+            {
+                ApiKey = "AIzaSyBIGPP5wtt3bGl4aBMiuMsPiyJxVlbe9Js",
+                AuthDomain = "smarthome-50f1c.firebaseapp.com",
+                Providers = new FirebaseAuthProvider[]
+                {
+                    new EmailProvider()
+                }
+            };
+
+            try
+            {
+                var client = new FirebaseAuthClient(config);
+                var user = await client.SignInWithEmailAndPasswordAsync(UserLoginEmail.Text, UserLoginPassword.Text);
+                await DisplayAlert("Login", "Erfolgreich!", "OK");
+            }
+            catch (Exception ex) {
+                await DisplayAlert("Login", ex.ToString(), "So sad");
+                Console.WriteLine(ex);
+            }
         }
     }
 }
